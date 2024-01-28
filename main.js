b89698148075e00a6170d1ef47f9914f82ed09e6
codecommit-test-user <register@yuyakawai.com>
2024-01-28 17:21:52 +0900

    Update fortuneList with new fortune

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const canvas = {
   height: 480,
 };
 
-const fortuneList = ["大吉", "中吉", "小吉", "吉", "凶", "大凶"];
+const fortuneList = ["大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"];
 
 let renderer = null;
 let scene = null;
